00eabf7d2d81b11e3ba2887f07e91aab5115b30e
Nikita5796 <67584544+Nikita5796@users.noreply.github.com>
2020-08-04 10:42:10 +0530

    Book Details added

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -6,10 +6,11 @@ import { bookRoutes } from './book.routes';
 import { BookService } from './book.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BooklistComponent } from './booklist/booklist.component';
+import { BookdetailsComponent } from './bookdetails/bookdetails.component';
 
 
 @NgModule({
-  declarations: [ BooklistComponent],
+  declarations: [ BooklistComponent, BookdetailsComponent],
   imports: [BrowserModule, RouterModule.forChild(bookRoutes), HttpClientModule],
   exports: [BooklistComponent],
   providers: [BookService],
--- src/app/book/book.routes.ts
+++ src/app/book/book.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookdetailsComponent } from './bookdetails/bookdetails.component';
 
 export const bookRoutes: Routes = [
+    { path: 'booklist', component: BooklistComponent },
+    {path : 'book/:id',component : BookdetailsComponent}
 ];
