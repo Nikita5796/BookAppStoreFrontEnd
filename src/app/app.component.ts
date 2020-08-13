436a80e422874f7b5871a8a15887c1c5d823a100
Nikita5796 <67584544+Nikita5796@users.noreply.github.com>
2020-08-13 12:04:03 +0530

    Login Logout button Toggle
    hide login when user is actually login into application and vice-versa.
    wlcome + username added.
    addbook link added into navigation bar.
    Cart button removed.
    authgard from home link removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnChanges, SimpleChanges, AfterViewChecked } from '@angular/core';
+import { Router } from '@angular/router';
+import { userInfo } from './customer/login/userInfo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewChecked{
   title = 'bookapp';
+  userInformation : userInfo = new userInfo();
+  userId : number;
+  static status : boolean = false;
+
+  @ViewChild('userProfile',{ static: true }) userProfile : ElementRef;
+  @ViewChild('loginBtn',{ static: true }) loginBtn : ElementRef;
+  @ViewChild('logOutBtn',{ static: true }) logOutBtn : ElementRef;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.display();
+  }
+
+  ngAfterViewChecked(){
+    if (localStorage.getItem('userID') && !AppComponent.status ){
+      this.display();
+      this.printlog();
+      AppComponent.status = true;
+    }   
+  }
+
+  display(){
+    this.userId = +localStorage.getItem('userID');
+    if(this.userId != 0)
+    {
+      this.userInformation.userId = +localStorage.getItem('userID');
+      this.userInformation.userName = localStorage.getItem('username');
+      this.userInformation.token = localStorage.getItem('userToken');
+      this.printlog();
+    }   
+  }
+
+  logOut() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('userID');
+    localStorage.removeItem('userToken');
+    this.userProfile.nativeElement.classList.add('d-none');
+    this.loginBtn.nativeElement.classList.remove('d-none');
+    this.logOutBtn.nativeElement.classList.add('d-none');
+    this.router.navigate(['login']);
+    AppComponent.status = false;
+    
+  }
+
+  printlog () {
+    this.userProfile.nativeElement.classList.remove('d-none');
+    this.loginBtn.nativeElement.classList.add('d-none');
+    this.logOutBtn.nativeElement.classList.remove('d-none');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BooklistComponent } from './book/booklist/booklist.component';
-import { AuthGuard } from './auth/auth.guard';
-
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
   { path: 'booklist', component: BooklistComponent }
